2869387fa986e0e487876ccbc68374c57e09b76f
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-10-07 13:28:55 +0200

    lab solved with bonus

--- src/viking.js
+++ src/viking.js
@@ -40,4 +40,53 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    } 
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    } 
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack() {
+        let randomSaxIndex = Math.floor(this.saxonArmy.length * Math.random())
+        let randomSaxon = this.saxonArmy[randomSaxIndex];
+        let randomViking = this.vikingArmy[Math.floor(this.vikingArmy.length * Math.random())];
+        let result = randomSaxon.receiveDamage(randomViking.strength);
+        if (randomSaxon.health <= 0) {
+            this.saxonArmy.splice(randomSaxIndex,1)
+        }
+        return result;
+    } 
+    saxonAttack() {
+        let randomVikIndex = Math.floor(this.vikingArmy.length * Math.random())
+        let randomViking = this.vikingArmy[randomVikIndex];
+        let randomSaxon = this.saxonArmy[Math.floor(this.saxonArmy.length * Math.random())]
+        let result = randomViking.receiveDamage(randomSaxon.strength);
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice(randomVikIndex,1)
+        }
+        return result;
+    }
+    showStatus() {
+        if (this.saxonArmy.length ===0) {
+            return "Vikings have won the war of the century!"
+        } else if (this.vikingArmy.length ===0)  {
+            return "Saxons have fought for their lives and survived another day..."
+        } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
+}
+/* let war1 = new War;
+let viking1 = new Viking("John", 30, 30)
+let saxon1 = new Saxon(30, 30)
+console.log(war1.addViking(viking1));
+console.log(war1.addSaxon(saxon1));
+console.log(war1.saxonAttack());
+console.log(war1.showStatus()) */
+
+
